e7b1ea33d7ed21a674c69614b7e226e9b4110da6
omars <pakatsy@ciencias.unam.mx>
2017-06-16 12:21:48 -0500

    este es el bueno

--- test/err6.s
+++ test/err6.s
@@ -4,3 +4,4 @@ main:               ; Inicia el codigo del programa
 li $a1, 5         ; a1=0
 li $a0, 20                       ; Codigo erroneo
 syscall                         ; Llamada al sistema (imprime entero)
+
